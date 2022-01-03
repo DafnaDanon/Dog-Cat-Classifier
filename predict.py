ded03b8b37bd8c0de3f60e69c6949a9f7328532d
DafnaDanon <danon.dafna@gmail.com>
2022-01-03 15:11:41 +0200

    Task 2. Accept video

--- predict.py
+++ predict.py
@@ -1,6 +1,9 @@
 # Arda Mavi
-
 import os.path
+import cv2
+import numpy as np
+from scipy.misc import imresize
+
 from keras.models import Sequential
 from keras.models import model_from_json
 
@@ -10,16 +13,9 @@ def predict(model, X):
     Y = 'cat' if Y[0] == 0 else 'dog'
     return Y
 
-if __name__ == '__main__':
-    import sys
-    img_dir = sys.argv[1]
-    if not os.path.exists('output'):
-        os.mkdir('output')
-    from get_dataset import get_img
-    img = get_img(img_dir)
-    import numpy as np
+def check_frame(name, img):
     X = np.zeros((1, 64, 64, 3), dtype='float64')
-    X[0] = img
+    X[0] = imresize(img, (64, 64, 3))
     # Getting model:
     model_file = open('Data/Model/model.json', 'r')
     model = model_file.read()
@@ -28,7 +24,40 @@ if __name__ == '__main__':
     # Getting weights
     model.load_weights("Data/Model/weights.h5")
     Y = predict(model, X)
-    # append result to file
-    with open('output/task1.txt', 'a') as f:
-        f.write(img_dir + ': ' + Y + '\n')
-    print('It is a ' + Y + ' !')
+    return Y
+
+if __name__ == '__main__':
+    import sys
+    img_dir = sys.argv[1]
+    if not os.path.exists('output'):
+        os.mkdir('output')
+    filename, file_extension = os.path.splitext(img_dir)
+    frame_count = 1
+    if file_extension.__eq__('.mp4'):
+        cap = cv2.VideoCapture(img_dir)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            try:
+                cv2.imshow('frame', frame)
+                Y = check_frame(img_dir, frame)
+                # append result to file
+                with open('output/task2.txt', 'a') as f:
+                    f.write(img_dir + str(frame_count) + ': ' + Y + '\n')
+                print('It is a ' + Y + ' !')
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            except:
+                print('frame ' + str(frame_count) + 'was not readable')
+                break
+            frame_count = frame_count + 1
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        from get_dataset import get_img
+        img = get_img(img_dir)
+        Y = check_frame(img_dir, img)
+        # append result to file
+        with open('output/task2.txt', 'a') as f:
+            f.write(img_dir + ': ' + Y + '\n')
+        print('It is a ' + Y + ' !')
+
